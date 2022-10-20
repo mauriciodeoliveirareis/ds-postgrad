731c51e750ccaaae775cffa1edcc293b710967aa
mauricio.reis <mauricio.reis@workday.com>
2022-10-20 21:42:19 +0100

    finished all work form week 5

---
+++ workingWithData/week5_data_manipulation_in_R_AND_pandas_and_numpy/babyData/Snip.R
@@ -0,0 +1,9 @@
+x <- data.frame(
+name = c('John', 'Paul', 'George', 'Ringo', 'Stuart', 'Pete'),
+instrument = c('guitar', 'bass', 'guitar', 'drums', 'bass', 'drums'),
+stringsAsFactors = F)
+  
+y <- data.frame(
+name = c('John', 'Paul', 'George', 'Ringo', 'Brian'),
+band = c(T, T, T, T, F),
+stringsAsFactors = F)
